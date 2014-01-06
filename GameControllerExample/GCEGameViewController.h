b65833b34663a281c3e14ee9da4e98b3065826c7
Tim Chilvers <tim@futureworkshops.com>
2014-01-06 15:29:52 +0000

    Empty Game View Controller

---
+++ GameControllerExample/GCEGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCEGameViewController.h
+//  GameControllerExample
+//
+//  Created by Tim Chilvers on 06/01/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCEGameViewController : UIViewController
+
+@end
---
+++ GameControllerExample/GCEGameViewController.m
@@ -0,0 +1,19 @@
+//
+//  GCEGameViewController.m
+//  GameControllerExample
+//
+//  Created by Tim Chilvers on 06/01/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import "GCEGameViewController.h"
+#import <GameController/GameController.h>
+
+@interface GCEGameViewController ()
+
+@end
+
+@implementation GCEGameViewController
+
+
+@end
