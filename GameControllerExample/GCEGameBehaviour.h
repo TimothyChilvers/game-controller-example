38707788c1f367265c21fa017d31b0361e83d63c
Tim Chilvers <tim@futureworkshops.com>
2014-01-07 14:31:11 +0000

    Add game behaviour

---
+++ GameControllerExample/GCEGameBehaviour.h
@@ -0,0 +1,13 @@
+//
+//  GCEGameBehaviour.h
+//  GameControllerExample
+//
+//  Created by Tim Chilvers on 07/01/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCEGameBehaviour : NSObject
+
+@end
---
+++ GameControllerExample/GCEGameBehaviour.m
@@ -0,0 +1,13 @@
+//
+//  GCEGameBehaviour.m
+//  GameControllerExample
+//
+//  Created by Tim Chilvers on 07/01/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import "GCEGameBehaviour.h"
+
+@implementation GCEGameBehaviour
+
+@end
