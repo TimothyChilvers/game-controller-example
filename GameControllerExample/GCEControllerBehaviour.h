78787217d727d6e069f66866663d69eeae91bf44
Tim Chilvers <tim@futureworkshops.com>
2014-01-07 15:44:43 +0000

    Add controller behaviour class

---
+++ GameControllerExample/GCEControllerBehaviour.h
@@ -0,0 +1,13 @@
+//
+//  GCEControllerBehaviour.h
+//  GameControllerExample
+//
+//  Created by Tim Chilvers on 07/01/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GCEControllerBehaviour : NSObject
+
+@end
---
+++ GameControllerExample/GCEControllerBehaviour.m
@@ -0,0 +1,13 @@
+//
+//  GCEControllerBehaviour.m
+//  GameControllerExample
+//
+//  Created by Tim Chilvers on 07/01/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import "GCEControllerBehaviour.h"
+
+@implementation GCEControllerBehaviour
+
+@end
