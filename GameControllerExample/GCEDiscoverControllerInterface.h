2271790c8ab5d4fc6f057aad9572f7f47e052e64
Tim Chilvers <tim@futureworkshops.com>
2014-01-07 13:35:32 +0000

    Add GCEDiscoverControllerInterface

---
+++ GameControllerExample/GCEDiscoverControllerInterface.h
@@ -0,0 +1,15 @@
+//
+//  GCEDiscoverControllerInterface.h
+//  GameControllerExample
+//
+//  Created by Tim Chilvers on 07/01/2014.
+//  Copyright (c) 2014 Tim Chilvers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <GameController/GameController.h>
+
+@interface GCEDiscoverControllerInterface : NSObject
+
+
+@end
